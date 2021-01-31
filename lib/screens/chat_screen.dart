6b88a43c1fe1dec46ee938b492cddb273d2af37a
DESKTOP-1KAV3HP\pc <95.cksn@gmail.com>
2021-01-31 19:54:17 +0900

    firebase deploy Err clear.!
    firebase deploy했을 때 오류나는거 드디어 해결 ㅜㅜ
    따옴표가 아닌 "쌍따옴표를 사용했어야 했다.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:udemy_chat/widgets/chat/messages.dart';
 import 'package:udemy_chat/widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final fbm = FirebaseMessaging();
+    fbm.configure(
+      onMessage: (msg) {
+        //앱이 실행중일때 실행됨.
+        print('onMessage >>>> $msg');
+        return;
+      },
+      onLaunch: (msg) {
+        //앱이 꺼져있을때 실행됨.
+        print('onLaunch >>>> $msg');
+        return;
+      },
+      onResume: (msg) {
+        //앱이 대기중일때 실행됨.
+        print('onResume >>>> $msg');
+        return;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
